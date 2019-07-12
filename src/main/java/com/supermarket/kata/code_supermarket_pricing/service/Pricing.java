6fd6b15a7cea17d6aecf2a0a4867e11d0465a49d
Mohamed NASRI <mohamed.nasri@ineat-conseil.fr>
2019-07-12 15:41:50 +0200

    add computing the unit price

--- src/main/java/com/supermarket/kata/code_supermarket_pricing/service/Pricing.java
+++ src/main/java/com/supermarket/kata/code_supermarket_pricing/service/Pricing.java
@@ -1,5 +1,14 @@
 package com.supermarket.kata.code_supermarket_pricing.service;
 
+import com.supermarket.kata.code_supermarket_pricing.Discount;
+import com.supermarket.kata.code_supermarket_pricing.UnitPrice;
+
 public class Pricing {
-	
+
+	/** Calcul prix unitaire **/
+
+	public static float getPrice(UnitPrice price, Integer qte) {
+		return price.getAmout() * qte;
+	}
+
 }
